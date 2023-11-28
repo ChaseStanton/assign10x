133b7c0a484db6160457a8439218ceaeddab69a5
Reece Kalmar <ralmarkalmar@gmail.com>
2023-11-28 13:43:01 -0700

    s

--- BinaryMaxHeap.java
+++ BinaryMaxHeap.java
@@ -5,203 +5,190 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public class BinaryMaxHeap<E> implements PriorityQueue<E> {
-		private E[] backing;
-	    private int size;
-	    private int maxSize;
-	    private Comparator<? super E> custom;
+	private E[] backing;
+	private int size;
+	private int maxSize;
+	private Comparator<? super E> custom;
 
-	    @SuppressWarnings("unchecked")
-		public BinaryMaxHeap(){
-	        backing = (E[]) new Object[16];
-	        maxSize = 16;
-	        size = 0;
-	    }
-
-	    @SuppressWarnings("unchecked")
-		public BinaryMaxHeap(Comparator<? super E> custom){
-	        this.custom = custom; 
-	        backing = (E[]) new Object[16];
-	        maxSize = 16;
-	        size = 0;
-	    }
-	    @SuppressWarnings("unchecked")
-		public BinaryMaxHeap(List<? extends E> list){
-	    	
-
-	        this.maxSize = list.size();
-	        this.size = maxSize;
-	        this.backing = (E[]) new Object[maxSize + 1]; 
-
-	        int i = 1;
-	        for (E item : list) {
-	            backing[i++] = item;
-	        }
-
-	        buildHeap();
-	    }
-	    @SuppressWarnings("unchecked")
-		public BinaryMaxHeap(List<? extends E> list, Comparator<? super E> custom){
-	    	
-
-	        this.custom = custom;
-	        this.maxSize = list.size();
-	        this.size = maxSize;
-	        this.backing = (E[]) new Object[maxSize + 1];
-
-	        int i = 1;
-	        for (E item : list) {
-	            backing[i++] = item;
-	        }
-
-	        buildHeap();
-	    }
-	    	
-	    
-
-
-	    
-	    private void buildHeap() {
-	    	for(int i = size/2; i >= 1; i--)
-	    		percolateDown(i);
-	    		
-	    }
-	    
-
-		private void percolateDown(int index) {
-	    	int leftChild, rightChild, maxIndex;
-	        while ((2 * index) <= size) {
-	            leftChild = 2 * index;
-	            rightChild = 2 * index + 1;
-	            maxIndex = index;
-
-	            if (leftChild <= size && innerCompare(leftChild, maxIndex) > 0) {
-	                maxIndex = leftChild;
-	            }
-	            if (rightChild <= size && innerCompare(rightChild, maxIndex) > 0) {
-	                maxIndex = rightChild;
-	            }
-	            if (maxIndex != index) {
-	            	E temp = backing[index];
-	    	        backing[index] = backing[maxIndex];
-	    	        backing[maxIndex] = temp;
-	                index = maxIndex;
-	            } else {
-	                break;
-	            }
-	        }
-	    }
-
-
-@Override
-public void add(E item) {
-	if(size >= backing.length - 1) {
-        @SuppressWarnings("unchecked")
-        E[] temp = (E[]) new Object[backing.length * 2];
-        for (int i = 1; i <= size; i++) {
-            temp[i] = backing[i];
-        }
-        backing = temp;
-    }
-    size++;
-    backing[size] = item;
-    
-    percolateUp(size);
-	
-	}
-	
-
-private void percolateUp(int index) {
-	while (index > 1) {
-        int parentIndex = index / 2;
-        if (innerCompare(index, parentIndex) > 0) {
-            E temp = backing[index];
-            backing[index] = backing[parentIndex];
-            backing[parentIndex] = temp;
-            index = parentIndex;
-        } else {
-            break;
-        }
-    }
-}
-
-@SuppressWarnings("unchecked")
-private int innerCompare(int a, int b) {
-	if (custom != null) {
-        return custom.compare((E) backing[a], (E) backing[b]);
-    } else {
-        Comparable<? super E> comparableA = (Comparable<? super E>) backing[a];
-        Comparable<? super E> comparableB = (Comparable<? super E>) backing[b];
-        
-        if (comparableA != null && comparableB != null) {
-            return comparableA.compareTo((E) backing[b]);
-        } else {
-            if (comparableA == null && comparableB == null) {
-                return 0; 
-            } else if (comparableA == null) {
-                return -1; 
-            } else {
-                return 1; 
-            }
-        }
-    }
-}
-
-
-@Override
-public E peek() throws NoSuchElementException {
-	if(size == 0) {
-		throw new NoSuchElementException("The heap is empty");
-	}
-	return backing[1];
-}
-
-
-@Override
-public E extractMax() throws NoSuchElementException {
-	if(size == 0) {
-		throw new NoSuchElementException("The heap is empty");
-	}
-	E maxElement = backing[1];
-	backing[1] = backing[size];
-	size--;
-	
-	percolateDown(1);
-	
-	return maxElement;
-}
-
-
-@Override
-public int size() {
-	return size;
-}
-
-
-@Override
-public boolean isEmpty() {
-	if(size == 0)
-		return true;
-	return false;
-}
-
-
-@SuppressWarnings("unchecked")
-@Override
-public void clear() {
-	backing = (E[]) new Object[maxSize];
-    size = 0;
-
-}
-
-
-@Override
-public E[] toArray() {
 	@SuppressWarnings("unchecked")
-	E[] arr = (E[]) new Object[size];
-	for (int i = 0; i < size; i++) {
-        arr[i] = backing[i + 1];
-    }
-	return arr;
-}
+	public BinaryMaxHeap() {
+		backing = (E[]) new Object[16];
+		maxSize = 16;
+		size = 0;
+	}
+
+	@SuppressWarnings("unchecked")
+	public BinaryMaxHeap(Comparator<? super E> custom) {
+		this.custom = custom;
+		backing = (E[]) new Object[16];
+		maxSize = 16;
+		size = 0;
+	}
+
+	@SuppressWarnings("unchecked")
+	public BinaryMaxHeap(List<? extends E> list) {
+
+		this.maxSize = list.size();
+		this.size = maxSize;
+		this.backing = (E[]) new Object[maxSize + 1];
+
+		int i = 1;
+		for (E item : list) {
+			backing[i++] = item;
+		}
+
+		buildHeap();
+	}
+
+	@SuppressWarnings("unchecked")
+	public BinaryMaxHeap(List<? extends E> list, Comparator<? super E> custom) {
+
+		this.custom = custom;
+		this.maxSize = list.size();
+		this.size = maxSize;
+		this.backing = (E[]) new Object[maxSize + 1];
+
+		int i = 1;
+		for (E item : list) {
+			backing[i++] = item;
+		}
+
+		buildHeap();
+	}
+
+	private void buildHeap() {
+		for (int i = size / 2; i >= 1; i--)
+			percolateDown(i);
+
+	}
+
+	private void percolateDown(int index) {
+		int leftChild, rightChild, maxIndex;
+		while ((2 * index) <= size) {
+			leftChild = 2 * index;
+			rightChild = 2 * index + 1;
+			maxIndex = index;
+
+			if (leftChild <= size && innerCompare(leftChild, maxIndex) > 0) {
+				maxIndex = leftChild;
+			}
+			if (rightChild <= size && innerCompare(rightChild, maxIndex) > 0) {
+				maxIndex = rightChild;
+			}
+			if (maxIndex != index) {
+				E temp = backing[index];
+				backing[index] = backing[maxIndex];
+				backing[maxIndex] = temp;
+				index = maxIndex;
+			} else {
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void add(E item) {
+		if (size >= backing.length - 1) {
+			@SuppressWarnings("unchecked")
+			E[] temp = (E[]) new Object[backing.length * 2];
+			for (int i = 1; i <= size; i++) {
+				temp[i] = backing[i];
+			}
+			backing = temp;
+		}
+		size++;
+		backing[size] = item;
+
+		percolateUp(size);
+
+	}
+
+	private void percolateUp(int index) {
+		while (index > 1) {
+			int parentIndex = index / 2;
+			if (innerCompare(index, parentIndex) > 0) {
+				E temp = backing[index];
+				backing[index] = backing[parentIndex];
+				backing[parentIndex] = temp;
+				index = parentIndex;
+			} else {
+				break;
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private int innerCompare(int a, int b) {
+		if (custom != null) {
+			return custom.compare((E) backing[a], (E) backing[b]);
+		} else {
+			Comparable<? super E> comparableA = (Comparable<? super E>) backing[a];
+			Comparable<? super E> comparableB = (Comparable<? super E>) backing[b];
+
+			if (comparableA != null && comparableB != null) {
+				return comparableA.compareTo((E) backing[b]);
+			} else {
+				if (comparableA == null && comparableB == null) {
+					return 0;
+				} else if (comparableA == null) {
+					return -1;
+				} else {
+					return 1;
+				}
+			}
+		}
+	}
+
+	@Override
+	public E peek() throws NoSuchElementException {
+		if (size == 0) {
+			throw new NoSuchElementException("The heap is empty");
+		}
+		return backing[1];
+	}
+
+	@Override
+	public E extractMax() throws NoSuchElementException {
+		if (size == 0) {
+			throw new NoSuchElementException("The heap is empty");
+		}
+		E maxElement = backing[1];
+		backing[1] = backing[size];
+		size--;
+
+		percolateDown(1);
+
+		return maxElement;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if (size == 0)
+			return true;
+		return false;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void clear() {
+		backing = (E[]) new Object[maxSize];
+		size = 0;
+
+	}
+
+	@Override
+	public E[] toArray() {
+		@SuppressWarnings("unchecked")
+		E[] arr = (E[]) new Object[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = backing[i + 1];
+		}
+		return arr;
+	}
 
 }
--- PriorityQueue.java
+++ PriorityQueue.java
@@ -4,8 +4,8 @@ import java.util.NoSuchElementException;
 
 /**
  * This interface represents the priority queue abstract data type,
- * defining the operations and running times expected of any data 
- * structure used to implement a priority queue.  
+ * defining the operations and running times expected of any data
+ * structure used to implement a priority queue.
  * 
  * NOTE: The item with the highest priority is the "maximum" item.
  * 
@@ -15,7 +15,7 @@ import java.util.NoSuchElementException;
  * @param <E>
  */
 public interface PriorityQueue<E> {
-	
+
 	/**
 	 * Adds the given item to this priority queue.
 	 * O(1) in the average case, O(log N) in the worst case
@@ -23,7 +23,7 @@ public interface PriorityQueue<E> {
 	 * @param item
 	 */
 	public void add(E item);
-	
+
 	/**
 	 * Returns, but does not remove, the maximum item this priority queue.
 	 * O(1)
@@ -32,7 +32,7 @@ public interface PriorityQueue<E> {
 	 * @throws NoSuchElementException if this priority queue is empty
 	 */
 	public E peek() throws NoSuchElementException;
-	
+
 	/**
 	 * Returns and removes the maximum item this priority queue.
 	 * O(log N)
@@ -47,27 +47,27 @@ public interface PriorityQueue<E> {
 	 * O(1)
 	 */
 	public int size();
-	
+
 	/**
 	 * Returns true if this priority queue is empty, false otherwise.
 	 * O(1)
 	 */
 	public boolean isEmpty();
-	
+
 	/**
 	 * Empties this priority queue of items.
 	 * O(1)
 	 */
 	public void clear();
-	
-	/** 
+
+	/**
 	 * Creates and returns an array of the items in this priority queue,
 	 * in the same order they appear in the backing array.
 	 * O(N)
 	 * 
-	 * (NOTE: This method is needed for grading purposes. The root item 
-	 * must be stored at index 0 in the returned array, regardless of 
-	 * whether it is in stored there in the backing array.) 
+	 * (NOTE: This method is needed for grading purposes. The root item
+	 * must be stored at index 0 in the returned array, regardless of
+	 * whether it is in stored there in the backing array.)
 	 */
 	public Object[] toArray();
 }
