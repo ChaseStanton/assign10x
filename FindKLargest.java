f98042d1929d45b26604b606577e15bb7565ccd0
ChaseStanton <stantonchase4217@gmail.com>
2023-11-28 13:29:18 -0700

    dfg

--- FindKLargest.java
+++ FindKLargest.java
@@ -1,5 +1,7 @@
 package assign10;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -21,8 +23,23 @@ public class FindKLargest {
 	 * @throws IllegalArgumentException if k is negative or larger than the size of the given list
 	 */
 	public static <E extends Comparable<? super E>> List<E> findKLargestHeap(List<E> items, int k) throws IllegalArgumentException {
+	if(items.size() == 0)
 		return null;
+	if(k > items.size() || k < 0) {
+		throw new IllegalArgumentException("k must be in between 0 and the size of the list");
 	}
+	
+	BinaryMaxHeap<E> heap = new BinaryMaxHeap<E>(items);
+	List<E> result = new ArrayList<>();
+    for (int i = 0; i < k; i++) {
+        result.add(heap.extractMax());
+    }
+    
+    return result;
+	
+	
+	}
+	
 
 	/**
 	 * Determines the k largest items in the given list, using a binary max heap.
@@ -34,8 +51,19 @@ public class FindKLargest {
 	 * @throws IllegalArgumentException if k is negative or larger than the size of the given list
 	 */
 	public static <E> List<E> findKLargestHeap(List<E> items, int k, Comparator<? super E> cmp) throws IllegalArgumentException {
-		return null;
-	}
+		if(items.size() == 0)
+			return null;
+		if(k > items.size() || k < 0) {
+			throw new IllegalArgumentException("k must be in between 0 and the size of the list");
+		}
+		
+		BinaryMaxHeap<E> heap = new BinaryMaxHeap<E>(items, cmp);
+		List<E> result = new ArrayList<>();
+	    for (int i = 0; i < k; i++) {
+	        result.add(heap.extractMax());
+	    }
+	    
+	    return result;	}
 
 	/**
 	 * Determines the k largest items in the given list, using Java's sort routine and the 
@@ -47,8 +75,22 @@ public class FindKLargest {
 	 * @throws IllegalArgumentException if k is negative or larger than the size of the given list
 	 */
 	public static <E extends Comparable<? super E>> List<E> findKLargestSort(List<E> items, int k) throws IllegalArgumentException {
-		return null;
-	}
+		if(items.size() == 0)
+			return null;
+		if(k > items.size() || k < 0) {
+			throw new IllegalArgumentException("k must be in between 0 and the size of the list");
+		}
+		
+		Collections.sort(items);
+		int startingIndex = items.size() - k;
+		while (startingIndex > 0) {
+            items.remove(0); 
+            startingIndex--;
+        }
+        
+        return items; 
+    }
+	
 
 	/**
 	 * Determines the k largest items in the given list, using Java's sort routine.
@@ -60,6 +102,18 @@ public class FindKLargest {
 	 * @throws IllegalArgumentException if k is negative or larger than the size of the given list
 	 */
 	public static <E> List<E> findKLargestSort(List<E> items, int k, Comparator<? super E> cmp) throws IllegalArgumentException {
-		return null;
-	}
+		if(items.size() == 0)
+			return null;
+		if(k > items.size() || k < 0) {
+			throw new IllegalArgumentException("k must be in between 0 and the size of the list");
+		}
+		items.sort(cmp);
+		int startingIndex = items.size() - k;
+		while (startingIndex > 0) {
+            items.remove(0); 
+            startingIndex--;
+        }
+        
+        return items; 
+    }
 }
